d93e764841b483d5be2a0dd2ba291d028d7eb7c1
BananaFalls <BananaFallsYT@gmail.com>
2020-03-25 16:43:12 -0700

    Update error and success messages
    This includes adding the error array (yes I know there are better ways to do that).

--- src/main/java/net/bnbdiscord/tpplugin/commands/TPRequestAcceptCommand.java
+++ src/main/java/net/bnbdiscord/tpplugin/commands/TPRequestAcceptCommand.java
@@ -17,6 +17,16 @@ public class TPRequestAcceptCommand implements CommandExecutor {
     public TPRequestAcceptCommand(TPPlugin p) {
         plugin = p;
     }
+    
+    String[] errors = {
+            ChatColor.RED + "" + ChatColor.BOLD + "AW SHUCKS! " + ChatColor.RED,
+            ChatColor.RED + "" + ChatColor.BOLD + "OOPS! " + ChatColor.RED,
+            ChatColor.RED + "" + ChatColor.BOLD + "ERROR! " + ChatColor.RED,
+            ChatColor.RED + "" + ChatColor.BOLD + "FAIL! " + ChatColor.RED,
+            ChatColor.RED + "" + ChatColor.BOLD + "UH OH! " + ChatColor.RED,
+            ChatColor.RED + "" + ChatColor.BOLD + "WHOOPS! " + ChatColor.RED,
+            ChatColor.RED + "" + ChatColor.BOLD + "NOPE! " + ChatColor.RED
+    };
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
@@ -26,51 +36,51 @@ public class TPRequestAcceptCommand implements CommandExecutor {
                 ArrayList<String> reqs = plugin.TeleportRequests.get(player.getUniqueId().toString());
                 if (reqs.size() > 1) {
                     if (args.length == 0) {
-                        player.sendMessage(ChatColor.RED + "You have multiple teleport requests:");
+                        player.sendMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + "PROBLEM! " + ChatColor.YELLOW + "You have multiple teleport requests:");
                         ArrayList<String> reqNames = new ArrayList<>();
                         for(String r : reqs){
                             reqNames.add(Objects.requireNonNull(plugin.getServer().getPlayer(UUID.fromString(r))).getName());
                         }
-                        player.sendMessage(ChatColor.RED + String.join(", ", reqNames));
-                        player.sendMessage(ChatColor.RED + "Therefore, you must specify someone to accept.");
+                        player.sendMessage(ChatColor.YELLOW + String.join(", ", reqNames));
+                        player.sendMessage(ChatColor.YELLOW + "Therefore, you must specify who to accept.");
                         return false;
                     } else if (args.length > 1) return false;
                     else {
                         Player teleportingPlayer = plugin.getServer().getPlayer(args[0]);
 
                         if (teleportingPlayer == null) {
-                            player.sendMessage(ChatColor.RED + "You invalid user");
+                            player.sendMessage(errors[new Random().nextInt(errors.length)] + "That player has not sent you a teleport request!");
                             return true;
                         }
 
                         if(!reqs.contains(teleportingPlayer.getUniqueId().toString())){
-                            player.sendMessage(ChatColor.RED + "Player has not requested a teleport.");
+                            player.sendMessage(errors[new Random().nextInt(errors.length)] + "That player has not sent you a teleport request!");
                             return true;
                         }
 
                         teleportingPlayer.teleport(player);
                         plugin.removeRequest(player.getUniqueId().toString(), teleportingPlayer.getUniqueId().toString());
-                        teleportingPlayer.sendMessage(ChatColor.AQUA + "You have been teleported!");
+                        teleportingPlayer.sendMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "TELEPORTED!" + ChatColor.GREEN + " You have been teleported!");
                     }   return true;
 
                 } else {
                     String req = reqs.get(0);
                     if(args.length != 0){
-                        player.sendMessage(ChatColor.RED + "You don't have multiple teleport requests. Use tpaccept without arguments");
+                        player.sendMessage(errors[new Random().nextInt(errors.length)] + "You don't have multiple teleport requests! Enter the command without any arguments.");
                         return true;
                     }
                     Player teleportingPlayer = plugin.getServer().getPlayer(UUID.fromString(req));
                     if(teleportingPlayer == null){
-                        player.sendMessage(ChatColor.RED + "That user doesn't exist anymore(?)");
+                        player.sendMessage(errors[new Random().nextInt(errors.length)] + "That player isn't online anymore!");
                         return true;
                     }
                     teleportingPlayer.teleport(player);
                     plugin.removeRequest(player.getUniqueId().toString(), req);
-                    teleportingPlayer.sendMessage(ChatColor.AQUA + "You have been teleported!");
+                    teleportingPlayer.sendMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "TELEPORTED!" + ChatColor.GREEN + " You have been teleported!");
                     return true;
                 }
             } else {
-                player.sendMessage(ChatColor.RED + "You don't have any pending teleport requests");
+                player.sendMessage(errors[new Random().nextInt(errors.length)] + "You don't have any pending teleport requests!");
                 return true;
             }
         }
