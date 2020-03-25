9ae8d33fc032010ebb345a7fed52291ea9f30110
BananaFalls <BananaFallsYT@gmail.com>
2020-03-25 16:53:23 -0700

    Update messages

--- src/main/java/net/bnbdiscord/tpplugin/commands/TPRequestDeclineCommand.java
+++ src/main/java/net/bnbdiscord/tpplugin/commands/TPRequestDeclineCommand.java
@@ -16,6 +16,16 @@ public class TPRequestDeclineCommand implements CommandExecutor {
     public TPRequestDeclineCommand(TPPlugin p) {
         plugin = p;
     }
+    
+    String[] errors = {
+            ChatColor.RED + "" + ChatColor.BOLD + "AW SHUCKS! " + ChatColor.RED,
+            ChatColor.RED + "" + ChatColor.BOLD + "OOPS! " + ChatColor.RED,
+            ChatColor.RED + "" + ChatColor.BOLD + "ERROR! " + ChatColor.RED,
+            ChatColor.RED + "" + ChatColor.BOLD + "FAIL! " + ChatColor.RED,
+            ChatColor.RED + "" + ChatColor.BOLD + "UH OH! " + ChatColor.RED,
+            ChatColor.RED + "" + ChatColor.BOLD + "WHOOPS! " + ChatColor.RED,
+            ChatColor.RED + "" + ChatColor.BOLD + "NOPE! " + ChatColor.RED
+    };
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
@@ -25,16 +35,16 @@ public class TPRequestDeclineCommand implements CommandExecutor {
                 ArrayList<String> reqs = plugin.TeleportRequests.get(player.getUniqueId().toString());
                 if (reqs.size() > 1) {
                     if (args.length == 0) {
-                        player.sendMessage(ChatColor.RED + "You have multiple teleport requests:");
-                        player.sendMessage(ChatColor.RED + String.join(", ", reqs));
-                        player.sendMessage(ChatColor.RED + "Therefore, you must specify someone to decline.");
+                        player.sendMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + "PROBLEM! " + ChatColor.YELLOW + "You have multiple teleport requests:");
+                        player.sendMessage(ChatColor.YELLOW + String.join(", ", reqs));
+                        player.sendMessage(ChatColor.YELLOW + "Therefore, you must specify someone to decline.");
                         return true;
                     } else if (args.length > 1) return false;
                     else {
                         Player teleportingPlayer = plugin.getServer().getPlayer(args[0]);
 
                         if (teleportingPlayer == null) {
-                            player.sendMessage(ChatColor.RED + "You invalid user");
+                            player.sendMessage(errors[new Random().nextInt(errors.length)] + "That player has not sent you a teleport request!");
                             return true;
                         }
 
@@ -44,30 +54,30 @@ public class TPRequestDeclineCommand implements CommandExecutor {
                         }
 
                         plugin.removeRequest(player.getUniqueId().toString(), teleportingPlayer.getUniqueId().toString());
-                        teleportingPlayer.sendMessage(ChatColor.RED + "Your teleport request("+ player.getName() + ") has been declined");
-                        player.sendMessage(ChatColor.AQUA + "Done!");
+                        teleportingPlayer.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "DECLINED! " + ChatColor.RED + "Your teleport request to " + player.getName() + " has been declined :(");
+                        player.sendMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "DECLINED! " + ChatColor.GREEN + "Successfully declined " + teleportingPlayer.getName + "'s teleport request!");
                         return true;
                     }
 
                 } else {
                     String req = reqs.get(0);
                     if(args.length != 0){
-                        player.sendMessage(ChatColor.RED + "You don't have multiple teleport requests. Use tpdecline without arguments");
+                        player.sendMessage(errors[new Random().nextInt(errors.length)] + "You don't have multiple teleport requests! Enter the command without any arguments.");
                         return true;
                     }
                     Player teleportingPlayer = plugin.getServer().getPlayer(UUID.fromString(req));
                     if(teleportingPlayer == null){
-                        player.sendMessage(ChatColor.RED + "That user doesn't exist anymore(?)");
+                        player.sendMessage(errors[new Random().nextInt(errors.length)] + "That player isn't online anymore!");
                         return true;
                     }
                     plugin.removeRequest(player.getUniqueId().toString(), teleportingPlayer.getUniqueId().toString());
-                    teleportingPlayer.sendMessage(ChatColor.RED + "Your teleport request to "+ player.getName() + " has been declined");
-                    player.sendMessage(ChatColor.AQUA + "Done!");
+                    teleportingPlayer.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "DECLINED! " + ChatColor.RED + "Your teleport request to " + player.getName() + " has been declined :(");
+                    player.sendMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "DECLINED! " + ChatColor.GREEN + "Successfully declined " + teleportingPlayer.getName + "'s teleport request!");
                     return true;
 
                 }
             } else {
-                player.sendMessage(ChatColor.RED + "You don't have any pending teleport requests");
+                player.sendMessage(errors[new Random().nextInt(errors.length)] + "You don't have any pending teleport requests!");
             }
         }
         return false;
